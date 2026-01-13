70a48fcb3ede5ba40dd88be9a429e127a74a3b02
Yash Thepra <yash.thepra@intimetec.com>
2026-01-13 18:19:47 +0530

    Input validation for wrong data type and odd, even values is added.

--- YashThepra_week1_ButterflyPattern/ButterflyPattern.cpp
+++ YashThepra_week1_ButterflyPattern/ButterflyPattern.cpp
@@ -1,19 +1,71 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
+using std::string;
+
+void trimInputString(string &inputValue) {
+    int startIndex = 0;
+
+    while(startIndex < inputValue.length() && inputValue[startIndex] == ' ') startIndex++;
+    int endIndex = inputValue.length() - 1;
+
+    while(endIndex >= 0 && inputValue[endIndex] == ' ') endIndex--;
+    
+    int newIndex = 0;
+    while (startIndex <= endIndex) {
+        inputValue[newIndex++] = inputValue[startIndex++];
+    }
+    inputValue.resize(newIndex);
+}
+
+int isValidInput(string inputValue) {
+    if (inputValue.length() == 0) return 0;
+    for(int index = 0; index < inputValue.length(); index++) {
+        int temp = (int)(inputValue[index] - '0');
+       
+        if (temp < 0 || temp > 9)
+            return 0;
+
+        if (index == 0 && temp == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int stringToNum(string inputValue) {
+    int num = 0;
+    for(int index = 0; index < inputValue.length(); index++) {
+        num = (10 * num) + (int)(inputValue[index] - '0');
+    }
+    return num;
+}
 
 int main() {
-    int numberOfRows;
-    cout << "Enter the size (Row length should be odd) of the pattern: ";
-    cin >> numberOfRows;
-    cout << "\n";
+    string numColumnsOfSingleSide = "";
+    cout << "Enter the number of Columns for the Single side of the pattern: ";
+    while (1) {
+        cin >> numColumnsOfSingleSide;
+        cout << '\n';
+        trimInputString(numColumnsOfSingleSide);
+        if (isValidInput(numColumnsOfSingleSide)) break;
+        else {
+            cout << "Invalid Input! Try Again.\n";
+            getchar();
+        }
+    }
+
+    int numOfOneSideColumns = stringToNum(numColumnsOfSingleSide);
+    
+    int numberOfRows = (2 * numOfOneSideColumns) - 1;
+
     int halfNumberOfRows = numberOfRows / 2;
 
     for(int rowIndex = 1; rowIndex <= halfNumberOfRows;  rowIndex++) {
         int flag = 1;
         int columnIndex = 1;
-        
+
         while (columnIndex <= rowIndex) {
             cout << flag;
             if (flag) flag = 0;
@@ -59,5 +111,6 @@ int main() {
         }
         cout << "\n";
     }
+    return 0;
     return 0;
 }
